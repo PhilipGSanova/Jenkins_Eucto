9b5e6e4295efd2b622eb7da1d3295acace567d87
PhilipGSanova <sanov2004@gmail.com>
2025-10-03 13:26:18 +0530

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static String greet() {
+        return "Hello, Jenkins CI/CD!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(greet());
+    }
+}
